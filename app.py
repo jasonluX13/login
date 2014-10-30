41db1512cd96c5d6c191cabf88f84470906703fb
jasonluX13 <jasonluX13@gmail.com>
2014-10-30 11:44:43 -0400

    Index.html works

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from pymongo import Connection
 from flask import Flask, render_template,session,redirect
 
-app=Flask(__name__)
+app= Flask(__name__)
 
 @app.route("/")
 def index():
+    '''
     if 'n' not in session:
         session['n']=0
         
     n = session['n']
     n=n+1
     session['n']=n
-    
-    return render_template("index.html",n=n)
-
+    '''
+    return render_template("index.html")
+'''
 @app.route("/logout")
 def logout():
     session.pop('n',None)
     return redirect("/")
-
+'''
 
 if __name__=="__main__":
     app.debug=True
